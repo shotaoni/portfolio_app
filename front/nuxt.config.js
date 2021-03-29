30485f42f53b32b37345048f9537e85e3ccc993f
shota <shota.oni@git.com>
2021-03-29 17:41:19 +0900

    deploy-confirm21

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -32,6 +32,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    "plugins/axios",
     '@/plugins/vee-validate',
     '@/plugins/auth-check'
   ],
