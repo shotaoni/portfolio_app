ac125221b3cd74e4a046cf30e242ff2c3f8f8bae
shota <shota.oni@git.com>
2021-04-04 21:41:59 +0900

    add comment-search-func

---
+++ front/components/molecules/SearchComments.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card flat>
+    <v-card-title>
+      コメントを探す
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        v-model="commentword"
+        hint="探したいコメントの単語を入力してください"
+      >
+        <v-icon slot="prepend">
+          mdi-magnify
+        </v-icon>
+      </v-text-field>
+    </v-card-text>
+    <Comment
+      v-for="comment in comments"
+      :key="comment.id"
+      :comment="comment"
+    />
+  </v-card>
+</template>
+
+<script>
+import _ from 'lodash'
+import axios from '@/plugins/axios'
+import Comment from '~/components/molecules/Comment.vue'
+export default {
+  components: {
+    Comment
+  },
+  data () {
+    return {
+      comments: [],
+      commentword: ''
+    }
+  },
+  watch: {
+    commentword () {
+      if (this.commentword) {
+        this.delayFunc()
+      } else {
+        this.comments = []
+      }
+    }
+  },
+  created () {
+    this.delayFunc = _.debounce(this.search, 500)
+  },
+  methods: {
+    search () {
+      console.log(this.commentword)
+      axios
+        .get('v1/comments', {
+          params: {
+            title: this.commentword
+          }
+        })
+        .then((res) => {
+          this.comments = res.data
+        })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- front/pages/search.vue
+++ front/pages/search.vue
@@ -34,6 +34,12 @@
                 mdi-note-search-outline
               </v-icon>
             </v-tab>
+            <v-tab class="ma-0 pa-0">
+              コメント
+              <v-icon>
+                mdi-comment-text-multiple-outline
+              </v-icon>
+            </v-tab>
           </v-tabs>
           <v-tabs-items
             v-model="tab"
@@ -49,6 +55,11 @@
                 <SearchPosts />
               </v-card>
             </v-tab-item>
+            <v-tab-item>
+              <v-card flat>
+                <SearchComments />
+              </v-card>
+            </v-tab-item>
           </v-tabs-items>
         </v-card>
       </v-col>
@@ -59,10 +70,12 @@
 <script>
 import SearchUsers from '~/components/molecules/SearchUsers.vue'
 import SearchPosts from '~/components/molecules/SearchPosts.vue'
+import SearchComments from '~/components/molecules/SearchComments.vue'
 export default {
   components: {
     SearchUsers,
-    SearchPosts
+    SearchPosts,
+    SearchComments
   },
   data () {
     return {
