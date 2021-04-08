132caf6b0c18b0940e390a092f0746901ddb0d76
shota <shota.oni@git.com>
2021-04-08 20:40:07 +0900

    add like-post-ranking

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,14 +1,49 @@
 <template>
-  <div>
-    <PostFeed />
-  </div>
+ <v-container>
+  <v-row justify="center">
+      <v-col xl="2" lg="3" sm="8" cols="12">
+        <Menu
+          @openPostFeed="openPostFeed"
+          @openLikeRankingFeed="openLikeRankingFeed"
+        />
+      </v-col>
+      <v-col xl="5" lg="7" sm="8" cols="12">
+        <PostFeed
+        v-if="openpostFeed && !openlikeRankingFeed"
+        />
+        <LikeRankingFeed
+        v-if="!openpostFeed && !openlikeRankingFeed"
+        />
+      </v-col>
+    </v-row>
+ </v-container>
 </template>
 
 <script>
+import Menu from '~/components/molecules/Menu.vue'
 import PostFeed from '~/components/organisms/posts/PostFeed.vue'
+import LikeRankingFeed from '~/components/organisms/LikeRankingFeed.vue'
 export default {
   components: {
-    PostFeed
+    PostFeed,
+    LikeRankingFeed,
+    Menu
+  },
+  data () {
+    return {
+      openpostFeed: true,
+      openlikeRankingFeed: false
+    }
+  },
+  methods: {
+    openPostFeed () {
+      this.openpostFeed = true
+      this.openlikeRankingFeed = false
+    },
+    openLikeRankingFeed () {
+      this.openpostFeed = false
+      this.openlikeRankingFeed = false
+    }
   }
 }
 </script>
